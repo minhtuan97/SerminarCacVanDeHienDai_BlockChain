2584cbf56389ceadbd37f508b0b2191ed5afe9d5
minhtuan97 <minhtuanle97@gmail.com>
2019-12-20 17:00:07 +0700

    csdl

---
+++ mydb.sql
@@ -0,0 +1,32 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     12/20/2019 4:57:00 PM                        */
+/*==============================================================*/
+
+/*
+drop table if exists OFFCHAIN;
+
+drop table if exists ONCHAIN;
+*/
+/*==============================================================*/
+/* Table: OFFCHAIN                                              */
+/*==============================================================*/
+create table OFFCHAIN
+(
+   HASHFILE             char(64) not null  comment '',
+   FILE                 tinyint not null  comment '',
+   primary key (HASHFILE)
+);
+
+/*==============================================================*/
+/* Table: ONCHAIN                                               */
+/*==============================================================*/
+create table ONCHAIN
+(
+   HASH                 char(64) not null  comment '',
+   PREVHASH             char(64) not null  comment '',
+   TIMESTAMP            bigint not null  comment '',
+   DATA                 text not null  comment '',
+   primary key (HASH)
+);
+
