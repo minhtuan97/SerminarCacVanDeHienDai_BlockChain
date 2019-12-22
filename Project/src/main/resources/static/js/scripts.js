6a6907ab2938cff8e3283ee55600153e833af5d3
anhtuankg1997 <anhtuankg1997@gmail.com>
2019-12-22 17:28:56 +0700

    Them cac thanh vien vao Smart Contract

--- Project/src/main/resources/static/js/scripts.js
+++ Project/src/main/resources/static/js/scripts.js
@@ -18,8 +18,38 @@
 			}, 500);
 		}
 		hidePreloader();
+
 	});
 
+	$(document).ready(function() {
+	    if (window.location.hash) { 
+	        //bind to scroll function
+	        $(document).scroll( function() {
+	            var hash = window.location.hash
+	            var hashName = hash.substring(1, hash.length);
+	            var element;
+
+	            //if element has this id then scroll to it
+	            if ($(hash).length != 0) {
+	                element = $(hash);
+	            }
+	            //catch cases of links that use anchor name
+	            else if ($('a[name="' + hashName + '"]').length != 0)
+	            {
+	                //just use the first one in case there are multiples
+	                element = $('a[name="' + hashName + '"]:first');
+	            }
+
+	            //if we have a target then go to it
+	            if (element != undefined) {
+	                window.scrollTo(0, element.position().top);
+	            }
+	            //unbind the scroll event
+	            $(document).unbind("scroll");
+	        });
+	    }
+
+	});
 	
 	/* Navbar Scripts */
 	// jQuery to collapse the navbar on scroll
@@ -345,5 +375,11 @@
 	$(".button, a, button").mouseup(function() {
 		$(this).blur();
 	});
+	
+	
+
+	
 
 })(jQuery);
+
+
